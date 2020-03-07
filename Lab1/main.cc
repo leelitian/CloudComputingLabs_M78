b3bccd13144c30babf49aadbdf29e36d1504e2bf
leelitian <1623312249@qq.com>
2020-03-07 14:39:58 +0800

    single-thread

---
+++ Lab1/main.cc
@@ -0,0 +1,65 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+
+#include "sudoku.h"
+
+int64_t now()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
+int main(int argc, char *argv[])
+{
+    char file_path[128];
+
+    char puzzle[128];
+    int total_solved = 0;                            // 已解决的谜题总数
+    int total = 0;                                   // 谜题总数
+    bool (*solve)(int) = solve_sudoku_dancing_links; // 使用“舞蹈链”算法解决数独
+
+    // 读取文件名
+    char *_ = fgets(file_path, sizeof file_path, stdin);
+    if (file_path[strlen(file_path) - 1] == '\n')
+        file_path[strlen(file_path) - 1] = '\0';
+
+    int64_t start = now(); // 计时
+
+    FILE *fp = fopen(file_path, "r"); // 打开文件
+
+    while (fgets(puzzle, sizeof puzzle, fp) != NULL)
+    {
+        if (strlen(puzzle) >= N)
+        {
+            ++total;
+            // board = puzzle
+            input(puzzle);
+
+            if (solve(0))
+            {
+                ++total_solved;
+                // 检查结果是否正确
+                if (!solved())
+                    assert(0);
+                // 打印结果
+                for (int i = 0; i < N; ++i)
+                    printf("%d", board[i]);
+                printf("\n");
+            }
+            else
+            {
+                printf("No: %s", puzzle);
+            }
+        }
+    }
+
+    int64_t end = now();
+    double sec = (end - start) / 1000000.0;
+    printf("%f sec %f ms each %d\n", sec, 1000 * sec / total, total_solved);
+
+    return 0;
+}
---
+++ Lab1/sudoku_basic.cc
@@ -0,0 +1,86 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include <algorithm>
+
+#include "sudoku.h"
+
+int board[N];
+int spaces[N];
+int nspaces;
+int (*chess)[COL] = (int (*)[COL])board;
+
+static void find_spaces()
+{
+	nspaces = 0;
+	for (int cell = 0; cell < N; ++cell)
+	{
+		if (board[cell] == 0)
+			spaces[nspaces++] = cell;
+	}
+}
+
+// 将puzzle写入board
+void input(const char in[N])
+{
+	for (int cell = 0; cell < N; ++cell)
+	{
+		board[cell] = in[cell] - '0';
+		assert(0 <= board[cell] && board[cell] <= NUM);
+	}
+	find_spaces();
+}
+
+// 检查board是否为正确解
+bool solved()
+{
+	for (int row = 0; row < ROW; ++row)
+	{
+		// check row
+		int occurs[10] = {0};
+		for (int col = 0; col < COL; ++col)
+		{
+			int val = chess[row][col];
+			assert(1 <= val && val <= NUM);
+			++occurs[val];
+		}
+
+		if (std::count(occurs, occurs + 10, 1) != NUM)
+			return false;
+	}
+
+	for (int col = 0; col < COL; ++col)
+	{
+		int occurs[10] = {0};
+		for (int row = 0; row < ROW; ++row)
+		{
+			int val = chess[row][col];
+			// assert(1 <= val && val <= NUM);
+			++occurs[val];
+		}
+
+		if (std::count(occurs, occurs + 10, 1) != NUM)
+			return false;
+	}
+
+	for (int row = 0; row < ROW; row += 3)
+	{
+		for (int col = 0; col < COL; col += 3)
+		{
+			int occurs[10] = {0};
+			++occurs[chess[row][col]];
+			++occurs[chess[row][col + 1]];
+			++occurs[chess[row][col + 2]];
+			++occurs[chess[row + 1][col]];
+			++occurs[chess[row + 1][col + 1]];
+			++occurs[chess[row + 1][col + 2]];
+			++occurs[chess[row + 2][col]];
+			++occurs[chess[row + 2][col + 1]];
+			++occurs[chess[row + 2][col + 2]];
+
+			if (std::count(occurs, occurs + 10, 1) != NUM)
+				return false;
+		}
+	}
+	return true;
+}
